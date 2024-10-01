07011183d0eaf7f20f94e47f2ab2c55b134cd298
riku-kameda <ohacoriku0113@gmail.com>
2024-10-01 07:17:17 +0000

    unnchi
    unnchi

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -114,7 +114,7 @@ export function VideoPlayer() {
                     textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', // 文字の影で視認性向上
                 }}
             >
-                Tests
+                unnchi
             </div>
         </div>
     );
