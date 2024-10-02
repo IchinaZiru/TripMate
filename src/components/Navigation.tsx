c8f85f9a2fa8e2ced4f229f824b15a248d6644ae
IchinaZiru <yutaihuangjing08@gmail.com>
2024-10-02 16:30:36 +0000

    Navi
    add Navi

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navigation: React.FC = () => {
+
     return (
         <nav className="header-navigation">
             <Link to="/signin">Sign In</Link>
